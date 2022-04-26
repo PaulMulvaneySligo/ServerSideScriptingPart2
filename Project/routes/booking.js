1297676a0d0b7c9ed07401ee751bb07a8ea573c9
Paul Mulvaney <s00214107@mail.itsligo.ie>
2022-04-26 16:03:45 +0100

    nodemailer

--- Project/routes/booking.js
+++ Project/routes/booking.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { readBooking , createBooking, deleteBooking, updateBooking }= require('../models/booking.js');
 const router = express.Router();
+const nodemailer = require("nodemailer");
 
 router.get('/', async (req, res) => {
     res.render('booking')
@@ -12,6 +13,34 @@ router.post('/', async (req, res) => {
 
     req.session.flash = 
     { type: 'success', intro: 'Your Table Is Booked:', message:  "Table for <strong>" + req.body.name + "</strong> has been booked"}
+    
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'juoqmamgizgxokdf@ethereal.email',
+            pass: '2tgSvBTZpXn6deMwDS'
+        }
+    });
+
+
+    const emailBody = "From De'Villes: Booking has been created for " + req.body.name + ", for " + req.body.people+ ",at " + req.body.time +", on "+ req.body.date;
+
+    const msg = {
+        from: '"The Express App" <theExpressApp@example.com>', // sender address
+        to: "juoqmamgizgxokdf@ethereal.email", // list of receivers
+        subject: "Test", // Subject line
+        text: emailBody    
+    }
+    // send mail with defined transport object
+    const info = await transporter.sendMail(msg);
+
+    console.log("Message sent: %s", info.messageId);
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    
     
     res.redirect(303, '/booking/booked')
 })
